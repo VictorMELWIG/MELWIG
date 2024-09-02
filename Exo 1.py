0f9163e9e8a9e643e8b85c056b1dcdcd4142ba9d
VictorMELWIG <victor.melwig@gmail.com>
2024-09-02 17:57:14 +0200

    Delete Exo 1.py

--- Exo 1.py
+++
@@ -1,8 +0,0 @@
-a = int(input("a?"))
-if a > 30: 
-    print("La valeur est supérieur que 30")
-
-elif  a ==30 :
-    print("La valeur est egal a 30")
-else : 
-   print(" La valeur est inférieur a 30")
